5539d0809b04150e412a27d63a269debe21a0f45
Blueace-609 <110134556+Blueace-609@users.noreply.github.com>
2024-07-25 18:36:09 +0000

    lol

--- about_me/About_me.py
+++ about_me/About_me.py
@@ -12,6 +12,7 @@ st.image("about_me/images/FullSizeRender.jpg")
 st.caption("I'm on the left, the one on the right is my brother.")
 st.subheader("Age: 15")
 st.subheader("Living in: Saratoga, CA")
+st.subheader("Hobbies: Playing video games, scrolling on Wikipedia")
 st.subheader("Status: Tired")
 if st.checkbox("Favorite Game"):
     st.write("Splatoon 3")
