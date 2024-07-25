b3041f258c6a5482ad2ca72ecd8dba6a1ffe2c8e
Blueace-609 <110134556+Blueace-609@users.noreply.github.com>
2024-07-25 18:12:12 +0000

    change

--- about_me/About_me.py
+++ about_me/About_me.py
@@ -8,12 +8,12 @@ st.set_page_config(
 st.sidebar.success("There's more to me??")
 
 st.title("Hi! I'm Chris")
-st.image("./images/FullSizeRender.jpg")
+st.image("images/FullSizeRender.jpg")
 st.caption("I'm on the left, the one on the right is my brother.")
 st.subheader("Age: 15")
 st.subheader("Living in: Saratoga, CA")
 st.subheader("Status: Tired")
 if st.checkbox("Favorite Game"):
     st.write("Splatoon 3")
-    st.image("./images/download.jpg")
+    st.image("images/download.jpg")
 st.checkbox("Favorite color")
